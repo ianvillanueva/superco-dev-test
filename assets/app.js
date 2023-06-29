8063f01bd25c4519bc1a3d70e9107643c775f78d
Ian Villanueva <ianp.villanueva@gmail.com>
2023-06-29 21:37:49 +0800

    feat: implemented carousel for content cards and feature products

--- assets/app.js
+++ assets/app.js
@@ -24,6 +24,57 @@ $(document).ready(function () {
     $('.burger').toggleClass('active');
     $('.navigation').toggleClass('active');
   });
+  $('.cards-carousel').slick({
+    dots: false,
+    infinite: false,
+    draggable: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [{
+      breakpoint: 1380,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: false
+      }
+    }, {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: false
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        stagePadding: 100,
+        variableWidth: true
+      }
+    }]
+  });
+  var productsCarousel = $('.products-carousel').slick({
+    dots: false,
+    infinite: true,
+    draggable: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    centerMode: true
+  });
+  $('.btn-arrow-left').on('click', function (e) {
+    e.preventDefault();
+    productsCarousel.slick('slickPrev');
+  });
+  $('.btn-arrow-right').on('click', function (e) {
+    e.preventDefault();
+    productsCarousel.slick('slickNext');
+  });
 });
 $(window).on('load', function () {
   console.log("Load");
--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,65 @@ $(document).ready(() => {
         $('.burger').toggleClass('active');
         $('.navigation').toggleClass('active');
     })
+
+
+    $('.cards-carousel').slick({
+        dots: false,
+        infinite: false,
+        draggable: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1380,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    variableWidth: false,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    variableWidth: false,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    stagePadding: 100,
+                    variableWidth:true
+                }
+            }
+        ]
+      });
+
+      const productsCarousel = $('.products-carousel').slick({
+        dots: false,
+        infinite: true,
+        draggable: true,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+        centerMode:true,
+      });
+      $('.btn-arrow-left').on('click', (e) => {
+        e.preventDefault();
+        productsCarousel.slick('slickPrev');
+      })
+
+      $('.btn-arrow-right').on('click', (e) => {
+        e.preventDefault();
+        productsCarousel.slick('slickNext')
+      })
     
 })
 
